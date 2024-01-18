ba8d915932697178447028138cab5ed5e5e66262
gasht1 <gebriegashaw7@gmail.com>
2024-01-18 09:04:28 +0400

    array unshift

--- Array.js
+++ Array.js
@@ -8,7 +8,8 @@ console.log(arr);
 */
 const array = [1, 2, 34, 35, 36, 37, 38, 39];
 array.push(1);
-array.pop();
-array.pop();
-array.pop();
+array.unshift(1);
+//array.pop();
+//array.pop();
+//array.pop();
 console.log(array);
