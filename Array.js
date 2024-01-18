c61a620b299bda43c51eaf5d437d5c944a5fe585
gasht1 <gebriegashaw7@gmail.com>
2024-01-18 08:55:43 +0400

    array pop and push

--- Array.js
+++ Array.js
@@ -1,7 +1,14 @@
 /*const array = [1, 2, 34, 35, 36, 37, 38, 39];
 console.log(array.length);
-console.log(array);*/
+console.log(array);
 const arr = new Array();
 arr.length = 10;
 arr.fill(2);
 console.log(arr);
+*/
+const array = [1, 2, 34, 35, 36, 37, 38, 39];
+array.push(1);
+array.pop();
+array.pop();
+array.pop();
+console.log(array);
