0960df5f3021d1ae2e143cc66a526579fd9c8afe
gasht1 <gebriegashaw7@gmail.com>
2024-01-20 09:29:50 +0400

    array slice

--- Array.js
+++ Array.js
@@ -7,13 +7,15 @@ arr.fill(2);
 console.log(arr);
 */
 const array = [2, 34, 35, 36, 37, 38, 39];
-array.splice(2);
+const splice = array.splice(3, 6);
+const slice = array.slice(0, 2);
+console.log(slice);
 array.push(1);
 //array.unshift(39);
 //array.pop();
 //array.pop();
 //array.pop();
 
-console.log(`hello ${array}`);
+console.log(`hello ${splice}`);
 const age = 19;
 age > 18 ? console.log("you can drive") : console.log("you can not drive");
