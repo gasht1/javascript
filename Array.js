0caf0c9da877470fb523e6fb629e672e70365606
gasht1 <gebriegashaw7@gmail.com>
2024-01-20 08:36:20 +0400

    backticks function

--- Array.js
+++ Array.js
@@ -6,11 +6,11 @@ arr.length = 10;
 arr.fill(2);
 console.log(arr);
 */
-const array = [1, 2, 34, 35, 36, 37, 38, 39];
+const array = [2, 34, 35, 36, 37, 38, 39];
 array.push(1);
-array.unshift(1);
+array.unshift(39);
 //array.pop();
 //array.pop();
 //array.pop();
-console.log(array);
-console.log(array.length);
+
+console.log(`hello ${array}`);
