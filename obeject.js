aee1187992c32e5c2c77b1cfebc36b2c3f8963b1
gasht1 <gebriegashaw7@gmail.com>
2024-01-21 12:09:12 +0400

    set and get method

--- obeject.js
+++ obeject.js
@@ -2,12 +2,27 @@ const portfolio = {
   name: "Gashaw Gebrie",
   profession: ["   front-end developer  ", "maintenance"],
   rate: 5,
+  sayHello: () => {
+    console.log(hello);
+  },
+  get getRating() {
+    return this.rate;
+  },
+  set setMethod(value) {
+    return (this.rate = value);
+  },
 };
-delete portfolio.rate;
+portfolio.rate;
+portfolio.name;
+portfolio.getRating;
+portfolio.setMethod = 6;
+console.log(portfolio.getRating);
+
+/*delete portfolio.rate;
 portfolio.expert = "front-end expert";
 console.log(portfolio);
 console.log(portfolio["name"]);
 
 const obj = new Object();
 obj.name = "Gashaw Dreamer";
-console.log(obj);
+console.log(obj);*/
